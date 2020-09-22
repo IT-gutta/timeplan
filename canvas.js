aa6cfc0dd85a662aa577199d89a777ce406892ff
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2020-09-22 18:00:38 +0200

    ad

--- canvas.js
+++ canvas.js
@@ -3,20 +3,28 @@ const canvas = document.querySelector("canvas")
 const c = canvas.getContext("2d")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-const w = canvas.width
-const h = canvas.height
+let w = canvas.width
+let h = canvas.height
 
 
-const a = 500
-const inc = 0.1
-const moveStep = 0.001
+
+
+let a = w / 4
+let baseA = w / 4
+let inc = 0.1
+let moveStep = 0.001
 let offSet = 0
+let da = -0.5
 
 function loop(){
-
     requestAnimationFrame(loop)
+
     c.fillStyle = "black"
     c.fillRect(0, 0, w, h)
+
+    a += da
+    if(a < -baseA) da = 0.5
+    if(a > baseA) da = -0.5
 
     offSet += moveStep
     for(let x = 0 + offSet; x < 2*Math.PI + offSet; x+= inc){
@@ -27,4 +35,16 @@ function loop(){
         c.closePath()
     }
 }
-loop()
+
+
+function resizeCanvas(){
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    w = canvas.width
+    h = canvas.height
+    a = w / 4
+    baseA = a
+}
+
+
+window.addEventListener("resize", resizeCanvas)
